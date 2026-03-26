3bb9d079e59f68bdd5a5e72949f4d40d4567b910
ashishjauto <ashishjaiswalcs@gmail.com>
2026-03-26 10:26:15 +0530

    base test

---
+++ app/src/test/java/com/web/test/DemoWebTest.java
@@ -0,0 +1,19 @@
+package com.web.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoWebTest {
+/*
+    @Test
+    public void demoWebTest(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+
+        Assert.assertEquals("https://www.google.com/", driver.getCurrentUrl());
+
+
+    }*/
+}
